6abfdb62a7d69052ef21c647c213323506df5724
Daffa <daffamfzn@gmail.com>
2025-05-22 16:39:07 +0700

    feat: updated sequential scenarios (now complete)

--- packages/cli-ethers/src/index.ts
+++ packages/cli-ethers/src/index.ts
@@ -8,8 +8,8 @@ import {
   GAS_LIMIT,
   JUDGE_CONTRACT_NAME,
   LOCK_CONTRACT_NAME,
-  VERIFIER_CONTRACT_NAME,
   RPC_URL,
+  VERIFIER_CONTRACT_NAME,
 } from './constants'
 import { readContractABI, readContractAddress } from './utils'
 
@@ -19,28 +19,13 @@ var _ = require('lodash')
 
 dotenv.config()
 
-// Connect to the network
-const provider = new ethers.providers.JsonRpcProvider(RPC_URL)
+/* ----- CONTRACT DATA ----- */
 
-// Connect to the contract
+// Contract Definitions
 const factoryBroadcastFile = join(
   CONTRACT_DIR,
   'broadcast',
   `${FACTORY_CONTRACT_NAME}.s.sol`,
-  CHAIN_ID,
-  'run-latest.json'
-)
-const judgeBroadcastFile = join(
-  CONTRACT_DIR,
-  'broadcast',
-  `${JUDGE_CONTRACT_NAME}.s.sol`,
-  CHAIN_ID,
-  'run-latest.json'
-)
-const lockBroadcastFile = join(
-  CONTRACT_DIR,
-  'broadcast',
-  `${LOCK_CONTRACT_NAME}.s.sol`,
   CHAIN_ID,
   'run-latest.json'
 )
@@ -51,7 +36,6 @@ const verifierBroadcastFile = join(
   CHAIN_ID,
   'run-latest.json'
 )
-
 const factoryAbiFile = join(
   CONTRACT_DIR,
   'out',
@@ -70,19 +54,15 @@ const lockAbiFile = join(
   `${LOCK_CONTRACT_NAME}.sol`,
   `${LOCK_CONTRACT_NAME}.json`
 )
-const verifierAbiFile = join(
-  CONTRACT_DIR,
-  'out',
-  `${VERIFIER_CONTRACT_NAME}.sol`,
-  `${VERIFIER_CONTRACT_NAME}.json`
-)
 
+// Connect to the network
+const provider = new ethers.providers.JsonRpcProvider(RPC_URL)
 
-/* ------ Scenario ------ */
+/* ------ SCENARIO DATA ------ */
+
 // Initialize Participants Data
 const ACCOUNTS = await provider.listAccounts()
-
-console.log(ACCOUNTS)
+const PRICE = 80
 
 const BUYER = ACCOUNTS[0]
 const SELLER = ACCOUNTS[1]
@@ -93,14 +73,15 @@ const SELLER_SIGNER = provider.getSigner(SELLER)
 let BUYER_BALANCE = await provider.getBalance(BUYER)
 let SELLER_BALANCE = await provider.getBalance(SELLER)
 
+// console.log(`List of accounts: ${ACCOUNTS}`)
 console.log(`Initial buyer balance: ${ethers.utils.formatEther(BUYER_BALANCE)}`)
 console.log(
-  `Initial seller balance: ${ethers.utils.formatEther(SELLER_BALANCE)}\n`
+  `Initial seller balance: ${ethers.utils.formatEther(SELLER_BALANCE)}`
 )
 
-const PRICE = 80
+/* ----- CONTRACT SETUP ----- */
 
-// The Contract object
+// Factory Contract - Initialization
 // https://ethereum.stackexchange.com/a/134733
 const factoryContract = new ethers.Contract(
   readContractAddress(factoryBroadcastFile, FACTORY_CONTRACT_NAME),
@@ -109,7 +90,7 @@ const factoryContract = new ethers.Contract(
 )
 const factoryContractWithBuyer = factoryContract.connect(BUYER_SIGNER)
 
-// ----- Create Exchange -----
+// Factory Contract - Create Ex change
 const tx_createExchange = await factoryContractWithBuyer.createExchange(
   SELLER,
   PRICE,
@@ -117,12 +98,14 @@ const tx_createExchange = await factoryContractWithBuyer.createExchange(
     gasLimit: GAS_LIMIT,
   }
 )
-const receipt = await tx_createExchange.wait()
-const event = receipt.events?.find((e: any) => e.event === 'ExchangeCreate')
-
-const JUDGE_ADDRESS = event?.args?.judge
+const receipt_createExchange = await tx_createExchange.wait()
+const event_createExchange = receipt_createExchange.events?.find(
+  (e: any) => e.event === 'ExchangeCreate'
+)
+const JUDGE_ADDRESS = event_createExchange?.args?.judge
 console.log(`Judge address: ${JUDGE_ADDRESS}`)
 
+// Judge Contract - Initialization
 const judgeContractWithBuyer = new ethers.Contract(
   JUDGE_ADDRESS,
   readContractABI(judgeAbiFile),
@@ -133,79 +116,146 @@ const judgeContractWithSeller = new ethers.Contract(
   readContractABI(judgeAbiFile),
   SELLER_SIGNER
 )
+const JUDGE_SIGNER = provider.getSigner(JUDGE_ADDRESS)
 
-// ---- Seller Sets Verifier in Judge -----
-const VERIFIER_ADDRESS = readContractAddress(verifierBroadcastFile, VERIFIER_CONTRACT_NAME)
-await judgeContractWithSeller.setVerifier(VERIFIER_ADDRESS);
-console.log(`Verifier address: ${VERIFIER_ADDRESS}\n`);
+// Judge Contract - Set Verifer
+const VERIFIER_ADDRESS = readContractAddress(
+  verifierBroadcastFile,
+  VERIFIER_CONTRACT_NAME
+)
+const tx_setVerifier =
+  await judgeContractWithSeller.setVerifier(VERIFIER_ADDRESS)
+const receipt_setVerifier = await tx_setVerifier.wait()
+const event_setVerifier = receipt_setVerifier.events?.find(
+  (e: any) => e.event === 'ExchangeSetVerifier'
+)
+console.log(`Verifier address: ${VERIFIER_ADDRESS}`)
 
-// ----- Seller Creates Hash and C and Sends it to Buyer -----
-// _bytes32 Hash
+/* ----- SCENARIO START ----- */
+
+// Judge Contract - Check Status (Ensure it is uninitialized)
+let judgeStatus = await judgeContractWithBuyer.getStatus()
+console.log(`Judge status: ${judgeStatus}\n`)
+
+// 1. Seller creates hz and c and sends it to the Buyer through conventional methods of communication
+//    Hash is of type _bytes32
 const hashZ =
   '0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef' // replace with actual hash
 
-let judgeStatus  = await judgeContractWithBuyer.getStatus()
-console.log(judgeStatus)
+// 2. Buyer independently verifies hz, then sends hz and the payment to the Judge which in turn will create a Lock contract to lock the payment
 
-// ------ Buyer Independently Verifies Hz and Sends hz and Lock to Contract -----
-// Call init with payable amount = PRICE (make sure to send in wei)
+// Judge Contract - Init (w/ payable amount = PRICE (make sure to send in wei))
 const tx_Init = await judgeContractWithBuyer.init(hashZ, {
   value: ethers.utils.parseEther(PRICE.toString()), // or ethers.utils.parseUnits(PRICE.toString(), "wei") depending on unit
   gasLimit: GAS_LIMIT,
 })
+const receipt_Init = await tx_Init.wait()
+console.log('----- Buyer initializes exchange -----')
+console.log(`Init transaction mined: ${receipt_Init.transactionHash}`)
 
+// Check is Lock contract created
+const lockAddress = await judgeContractWithBuyer.getLockContract() // You don't need a signer for reading data
+console.log('Lock address:', lockAddress)
+
+// Lock Contract - Initialization
+const lockContract = new ethers.Contract(
+  lockAddress,
+  readContractABI(lockAbiFile),
+  provider
+)
+const lockContractWithJudge = lockContract.connect(JUDGE_SIGNER)
+
+// Check Balances
 BUYER_BALANCE = await provider.getBalance(BUYER)
 SELLER_BALANCE = await provider.getBalance(SELLER)
 
 console.log('\n----- Buyer creates Lock for payment -----')
-console.log(
-  `Create exchange buyer balance: ${ethers.utils.formatEther(BUYER_BALANCE)}`
-)
-console.log(
-  `Create exchange seller balance: ${ethers.utils.formatEther(SELLER_BALANCE)}\n`
-)
+console.log(`Buyer balance: ${ethers.utils.formatEther(BUYER_BALANCE)}`)
+console.log(`Seller balance: ${ethers.utils.formatEther(SELLER_BALANCE)}`)
 
-const receipt_Init = await tx_Init.wait()
+// Judge Contract - Check Status (Ensure it is initialized)
+judgeStatus = await judgeContractWithBuyer.getStatus()
+console.log(`Judge status: ${judgeStatus}\n`)
 
-console.log(`Init transaction mined: ${receipt_Init.transactionHash}\n`)
+// 3. Seller checks hz in the Judge contract with getHz, if it is the same, then the seller sends the proof parameters to the verify method and unlock the contract
 
+// Proof bytes — just 128 zero bytes (size depends on your circuit)
+const pa = [
+  '0x03704ee1383665cae109b3de9cc1ba0035a542a2cd6d88251aff58843afc1ff2',
+  '0x2db941c503ade2c1bb5d2c215e6ef5dbda0913f01aee4f7edcc2fe0e2b7c3947',
+]
+const pb = [
+  [
+    '0x043926fb95a0289bfec5c7df3d93378503fcff17cb054b3fd30c184f6221edcb',
+    '0x2fd17f632b4f6f1a2c43cb63529f5f6419a0da9d712dd9719c7d196d0ddbb913',
+  ],
+  [
+    '0x3060a91b8f21f8082d7257e5932f438a1cbc55b39bf714cd78f26cbda2828a87',
+    '0x013b0517c14fcbbca2da8b80c20e5a47c89f2ffa6674d74c16c5aa74dcb615d2',
+  ],
+]
+const pc = [
+  '0x2f46d99c56f65ab88e41de8ed2852d67962954f4fb6f943feeec327511b44838',
+  '0x10d7f02bd6ab4acc2baf277ee3af34db8c1d29350820a54c3d0edfcf4f9d1109',
+]
+const pubSignals = [
+  '0x0000000000000000000000000000000000000000000000000000000000000000',
+  '0x0000000000000000000000000000000000000000000000000000000000000001',
+]
 
-judgeStatus  = await judgeContractWithBuyer.getStatus()
-console.log(judgeStatus)
-
-// You don't need a signer for reading data
-const lockAddress = await judgeContractWithBuyer.getLockContract()
-console.log('\nLock address:', lockAddress)
-
-// You don't need a signer for reading data
-const sellerAddress = await judgeContractWithBuyer.getSeller()
-console.log('Seller address:', sellerAddress, '\n')
-
-// ----- Seller Checks Hz in Judge with getHz, if same Sends Proof to Verify and Unlock Contract -----
-// Dummy proof bytes — just 128 zero bytes (size depends on your circuit)
-// const dummyProof = ethers.utils.hexlify(new Uint8Array(128)) // or adjust size
-const pa = ["0x2d424f535f3162416a7ea4cb4e234877a06ec85e1c4b726173bea8f31bebba23", "0x007216c4cdb1cb3e6ab41dfb3ee98c25b7c5a01fc975a9de75bf1e02b7694001"]
-const pb = [["0x0ac4d88464a795b63c453560efb76b20b66c625d0f929763ae08468b1fe140bc", "0x1d6318b7a0b1601a6cd6c87729c27568ea41ee3357f51432b34441af04c6754f"],["0x2db4328dd056df6ad4d01bfc03990dc4010a41a03a5ab31d042cdcc5b41cb4ab", "0x2c5ef60033e243bcad674fba5bcb1b0bdb36b65fe14619e014dd54c359869d9c"]]
-const pc = ["0x067878595861072901551214e6d85706322d9c59f54eb57caa45e7e90ea0795e", "0x236b2db1ea9a7620b6afbc80296f9e9a3832e47d60ebc9e4f0fc060af3ffe25b"]
-const pubSignals = ["0x0000000000000000000000000000000000000000000000000000000000000000","0x0000000000000000000000000000000000000000000000000000000000000001"]
-
-// Dummy k — random bytes32
+// Dummy K — random bytes32
 const dummyK = ethers.utils.hexZeroPad('0x01', 32)
 
-const tx_verify = await judgeContractWithSeller.verify(pa, pb, pc, pubSignals, dummyK, {
-  gasLimit: GAS_LIMIT,
-})
+console.log('----- Check if we can obtain key prior to verify -----')
+// Check if the key is revealed in the Lock contract PRIOR to VERIFY
+try {
+  // Try to call getKey in a try-catch to see the detailed error
+  const revealedKey = await lockContractWithJudge.getKey()
+  console.log(`Key revealed: ${revealedKey}`)
+} catch (error: any) {
+  console.error('Key still not revealed!\n')
+}
+
+// Judge Contract - Verify
+const tx_verify = await judgeContractWithSeller.verify(
+  pa,
+  pb,
+  pc,
+  pubSignals,
+  dummyK,
+  {
+    gasLimit: GAS_LIMIT,
+  }
+)
 const receipt_verify = await tx_verify.wait()
-
-console.log('----- Seller sends K to smart contract and smart contract verifies: -----')
+console.log(
+  '----- Seller sends K to smart contract and smart contract verifies: -----'
+)
 console.log('Verify tx confirmed:', receipt_verify.transactionHash)
+judgeStatus = await judgeContractWithBuyer.getStatus()
+console.log('Judge status after verification:', judgeStatus)
 
+// Check if the key is revealed in the Lock contract AFTER VERIFY
+try {
+  // Try to call getKey in a try-catch to see the detailed error
+  const revealedKey = await lockContractWithJudge.getKey()
+  console.log(`Key revealed: ${revealedKey}`)
+} catch (error: any) {
+  console.error('\nError getting key:', error.message)
+}
+
+// Check Balances
 BUYER_BALANCE = await provider.getBalance(BUYER)
 SELLER_BALANCE = await provider.getBalance(SELLER)
 
+console.log('\n----- FINAL -----')
 console.log(`Final buyer balance: ${ethers.utils.formatEther(BUYER_BALANCE)}`)
-console.log(`Final seller balance: ${ethers.utils.formatEther(SELLER_BALANCE)}\n`)
+console.log(
+  `Final seller balance: ${ethers.utils.formatEther(SELLER_BALANCE)}\n`
+)
 
-
-judgeStatus  = await judgeContractWithBuyer.getStatus()
+// Judge Contract - Check Status (Ensure it is finished)
+judgeStatus = await judgeContractWithBuyer.getStatus()
 console.log(judgeStatus)
+
+// End of script
